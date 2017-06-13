a8f4649802b9441910874cec19c84945a16d77c4
HashiCorp Training <training@hashicorp.com>
2017-06-13 15:09:42 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,69 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-a4f9f2c2
+#
+# Your subnet ID is:
+#
+#     subnet-9a479ed3
+#
+# Your security group ID is:
+#
+#     sg-00100979
+#
+# Your Identity is:
+#
+#     hdays-michel-badger
+#
+
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "num_webs" {
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+terraform {
+  backend "atlas" {
+    name = "yannistheo/training"
+  }
+}
+
+resource "aws_instance" "web" {
+  # ...
+  ami                    = "ami-a4f9f2c2"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-9a479ed3"
+  vpc_security_group_ids = ["sg-00100979"]
+  count			 = "2"
+
+  tags {
+    "Identity" = "hdays-michel-badger"
+    "Person"   = "Yannis"
+    "Company"  = "Wowcher"
+    "Name"     = "web ${count.index+1}/${var.num_webs}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
